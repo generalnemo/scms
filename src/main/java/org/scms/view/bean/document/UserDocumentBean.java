2c7ee2df0a23e687c50d61957fb63c1013e366a1
Nemo <generalnemo@mail.ru>
2012-06-18 02:43:06 +0400

    documents versioning enabled

--- src/main/java/org/scms/view/bean/document/UserDocumentBean.java
+++ src/main/java/org/scms/view/bean/document/UserDocumentBean.java
@@ -3,13 +3,19 @@ package org.scms.view.bean.document;
 import java.util.Arrays;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 
+import org.primefaces.event.FileUploadEvent;
 import org.primefaces.event.NodeSelectEvent;
+import org.primefaces.model.UploadedFile;
 import org.scms.enumerate.ControlCategory;
 import org.scms.enumerate.citem.CItemType;
+import org.scms.model.entity.CItemRevision;
 import org.scms.view.bean.AbstractCItemBean;
+
+import com.ocpsoft.pretty.faces.application.PrettyRedirector;
 
 @Named("userDocument")
 @ViewScoped
@@ -28,25 +34,93 @@ public class UserDocumentBean extends AbstractCItemBean {
 		addProperty(PRETTY_EDIT, "pretty:editDocument");
 		addProperty(PRETTY_VIEW, "pretty:viewDocument");
 	}
-	
+
+	@Override
 	protected void pageLoad() {
-		super.pageLoad();
-		if (object!=null){
+		if (getObject() != null)
+			return;
+		if (objectId == null) {
+			createNewObject();
+			return;
+		}
+		try {
+			setObject(cItemService.findById(Long.parseLong(objectId)));
 			object.initObjectTreeNode();
+			CItemRevision revision = new CItemRevision();
+			revision.setcItem(object);
+			revision.setCreatedBy(userBean.getCurrentUser());
+			object.getRevisions().add(revision);
+		} catch (NumberFormatException e) {
+			PrettyRedirector.getInstance().redirect(fContext,
+					getProperty(PRETTY_404));
+			return;
+		}
+		if (object == null) {
+			PrettyRedirector.getInstance().redirect(fContext,
+					getProperty(PRETTY_404));
+			return;
 		}
 
 	}
-	
-	public void saveObject(){
-		if (object.getcCategory().isCc4()){
+
+	public void documentUploadListener(FileUploadEvent event) {
+		UploadedFile file = event.getFile();
+		if (file == null) {
+			return;
+		}
+		if (object.getId() == 0) {
+			super.documentUploadListener(event);
+			return;
+		}
+		int revisionsCount = object.getRevisions().size();
+		object.getRevisions().get(revisionsCount - 1)
+				.setData(file.getContents());
+		object.getRevisions().get(revisionsCount - 1)
+				.setContentType(file.getContentType());
+		object.getRevisions().get(revisionsCount - 1)
+				.setFileName(file.getFileName());
+	}
+
+	public void deleteUploadedDocument() {
+		int revisionsCount = object.getRevisions().size();
+		CItemRevision revision = object.getRevisions().get(revisionsCount - 1);
+		revision.setData(null);
+		revision.setContentType(null);
+		revision.setFileName(null);
+	}
+
+	public void saveObject() {
+		if (object.getcCategory().isCc4()) {
 			super.saveObject();
 			return;
 		}
-		
+		int revisionsCount = object.getRevisions().size();
+		if (object.getRevisions().get(revisionsCount - 1).getData() == null) {
+			fContext.addMessage(null, new FacesMessage(
+					FacesMessage.SEVERITY_ERROR,
+					"Необходимо загрузить файл документа", null));
+			return;
+		}
+		if (object.getSelectedCItemRevision() == null) {
+			fContext.addMessage(null, new FacesMessage(
+					FacesMessage.SEVERITY_ERROR,
+					"Необходимо выбрать версию документа", null));
+			return;
+		}
+		object.getRevisions()
+				.get(revisionsCount - 1)
+				.setPrevRevision(
+						object.getSelectedCItemRevision().getRevision());
+		for (CItemRevision revision : object.getRevisions()) {
+			revision.setCurrentRevision(false);
+		}
+		object.getRevisions().get(revisionsCount - 1).setCurrentRevision(true);
+		super.saveObject();
+
 	}
-	
+
 	public void onNodeSelectListener(NodeSelectEvent event) {
-        logger.info("in listener"); 
-    }
+		logger.info("in listener");
+	}
 
 }
